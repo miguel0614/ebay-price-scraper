f62784794109789dcb92b3db69fbb48ccad2b998
DylanTrom <60438776+DylanTrom@users.noreply.github.com>
2020-01-31 15:49:32 -0500

    Add files via upload
    Initial flask code

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("hello world")
+from flask import Flask, jsonify, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+# @app.route('/hello', methods=['GET'])
+# def hello_name():
+#     my_name = request.args['name']
+#     return 'Hello' + my_name + '.'
+
+
+# @app.route('/json', methods=['GET'])
+# def json():
+#     dict = {'some variable': 'something'}
+#     return jsonify(dict)
+
+if __name__=='__main__':
+    app.run(debug=True, host='localhost', port=8080)

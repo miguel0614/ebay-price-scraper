d9ac45d181fc2f6cefc0decda1d22743fef96461
Miguel Bautista <miguel.a.bautista@outlook.com>
2020-02-06 18:42:35 -0500

    Updated script to validate form before search

--- static/script.js
+++ static/script.js
@@ -1,7 +1,18 @@
 function validateForm() {
-  let x = document.forms["search"]["product"].value;
-  if (x === "") {
+  let name = document.forms["search"]["product"].value;
+  let priceLow = document.forms["search"]["price-min"].value;
+  let priceHigh = document.forms["search"]["price-max"].value;
+  if (name === "") {
     alert("Name must be filled out");
     return false;
   }
+    if (priceLow !== '' && priceHigh !== ''){
+      if (priceLow > priceHigh){
+        alert("Price range must go from least to greater price");
+        return false
+      }
+    }
+  return true
 }
+
+
